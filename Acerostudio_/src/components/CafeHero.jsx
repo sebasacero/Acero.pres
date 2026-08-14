254a10ddd294c7ecf66609d935c9d3d7bd6e8871
Sebastian Acero <139513184+sebasacero@users.noreply.github.com>
2026-08-14 08:31:30 -0500

    Delete Acerostudio_ directory

--- Acerostudio_/src/components/CafeHero.jsx
+++
@@ -1,7 +0,0 @@
-export default function CafeHero() {
-  return (
-    <div className="hero-image-container">
-      <img src="/image/local.jpg" alt="Foto del local ACEROPRESS" className="hero-bg-img" />
-    </div>
-  );
-}
--- Acerostudio_/src/components/WacBanner.jsx
+++
@@ -1,7 +0,0 @@
-export default function WacBanner() {
-  return (
-    <div className="product-banner">
-      <div className="banner-text-wac">WAC WAC WAC WAC WAC WAC WAC WAC WAC </div>
-    </div>
-  );
-}
